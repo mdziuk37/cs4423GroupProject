3e0bca184e58da251788d848e8badfaed43e5f6f
Vincent <pupper.bot+v@gmail.com>
2020-07-18 17:45:51 -0500

    Initial project creation
    Semi-empty project created with scenes corresponding to the different acts, or sections, of the game.
    Act 1 is the exposition
    Act 2 is the main character having a dream about the game he's about to be sucked into (allows players to pick their class, weapons just before Act 3)
    Act 3 is in-game exposition + story quests
    Act 4 is the boss fight/ending of the game

---
+++ GPROMM/Assets/Scripts/PauseMenuButtons.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/*
+ * This script contains the behavior for the pause menu
+ * buttons that appear when hitting the ESC key in-game.
+ */
+
+/*
+ * Meant to be used with script PauseMenuManager.cs
+ * where this script is to be components of the pause
+ * menu buttons, and the Manager script is the
+ * component of the Canvas that houses the buttons.
+ */
+ 
+public class PauseMenuButtons : MonoBehaviour
+{
+    public Canvas _menu;
+    
+    /*
+     * Resume game OnClick()
+     */
+    public void Resume()
+    {
+        _menu.enabled = !_menu.enabled; // Hide the menu
+        
+        Time.timeScale = 1.0f;
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    /*
+     * Restart game OnClick()
+     */
+    public void RestartGame()
+    {
+        Debug.Log("Restart");
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene(4);
+    }
+    
+    
+    // Exit to title screen
+    public void ReturnToTitle()
+    {
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ GPROMM/Assets/Scripts/PauseMenuManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/*
+ * This script contains the behavior for opening up the 
+ * pause menu in-game.
+ */
+
+/*
+ * Meant to be used with script PauseMenuButtons.cs
+ * where this script is the component of the Canvas that
+ * houses the buttons, and the Buttons script is
+ * to be components of the pause menu buttons.
+ */
+
+/*
+ * ###### NOTE: IF USING FIRSTPERSONCONTROLLER.CS FROM STANDARD ASSETS FOR UNITY ######
+ * --------------------------------------------------------------------------
+ *         Under the private method RotateView()
+ *         place a check for Time.deltaTime being 0.0f.
+ *         i.e.,
+ *                 if(Time.deltaTime == 0.0f){
+ *                     return;
+ *                  } 
+ * 
+ *         This check will prevent the camera from moving while
+ *         the game is paused.
+ */
+
+public class PauseMenuManager : MonoBehaviour
+{
+    private Canvas _menu;
+    public Camera firstPersonCamera;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _menu = GetComponent<Canvas>();
+        _menu.enabled = false;
+        // Just in case player does Pause->Exit->Start Game because it WILL stay at timeScale = 0.0f
+        Time.timeScale = 1.0f; 
+        //firstPersonCamera = gameObject.Find("FirstPersonCharacter").GetComponent(m_MouseLook);
+    }
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        // Show pause menu on ESC and hide it on ESC again
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            _menu.enabled = !_menu.enabled;
+            if (_menu.enabled == true)
+            {
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+                Time.timeScale = 0.0f;
+            }
+            else
+            {
+                //Cursor.lockState = CursorLockMode.None;
+                Cursor.lockState = CursorLockMode.Locked;
+                Time.timeScale = 1.0f;
+            }
+        }
+    }
+
+}
