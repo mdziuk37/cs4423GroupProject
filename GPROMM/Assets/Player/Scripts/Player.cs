aa1a4d641e6b00d9f7e1adf9197835bdab59f34f
Vincent <pupper.bot+v@gmail.com>
2020-08-06 05:55:55 -0500

    Title Screen + About/Credits Screens
    Start goes to Scene Act2 (scene moved from Player folder).
    Exit closes the program.
    Every scene following the title has a back button aside from Start (for now) and Exit (of course).
    TODO: Pause menu
    
    NOTE: Recommended to **build and run** project instead of just hitting play otherwise UI scaling errors may occur but shouldn't be permenant.

---
+++ GPROMM/Assets/Player/Scripts/Player.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    // the idea is to create a singleton player that will house the stats for the player. creating the player, leveling up, taking damage should use the functions in this class
+    // this will be passed to every level that is loaded
+
+
+    public int maxHealth;
+
+    public int playerClass;
+
+    //probably gonna nix this
+    public int mana;
+
+    public int strength;
+
+    public int wisdom;
+
+    public int dexterity;
+
+    public int damage;
+
+    public static Player instance;
+
+    public Player player;
+
+    private void Awake()
+    {
+        if (instance)
+        {
+            Debug.LogError("More than one Player in scene");
+        }
+        else
+        {
+            instance = this;
+
+
+        }
+    }
+
+    // use the menu to update the player class and then run create character
+    public void createCharacter()
+        {
+        //brute starting stats
+         if(playerClass == 1)
+            {
+            maxHealth = 150;
+            strength = 5;
+            wisdom = 1;
+            dexterity =3;
+            }
+         //wizard starting stats
+        if (playerClass == 2)
+        {
+            maxHealth = 100;
+            strength = 1;
+            wisdom = 5;
+            dexterity = 3;
+        }
+
+        //archer starting stats
+        if (playerClass == 3)
+        {
+            maxHealth = 120;
+            strength = 1;
+            wisdom = 3;
+            dexterity = 5;
+        }
+
+
+    }
+
+
+    public void levelUp()
+    {   
+        //brute level stats
+        if (playerClass == 1)
+        {
+            maxHealth += 15;
+            strength += 1;
+            
+        }
+        //wizard level stats
+        if (playerClass == 2)
+        {
+            maxHealth += 5;
+            wisdom += 1;
+           
+        }
+
+        //archer level stats
+        if (playerClass == 3)
+        {
+            maxHealth += 5;
+            dexterity += 10;
+        }
+
+
+
+
+    }
+
+
+   
+
+}
---
+++ GPROMM/Assets/Scripts/TitleScreenBehavior.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreenBehavior : MonoBehaviour
+{
+    /*
+     *                 FOR REFERENCE
+     * --------------------------------------------------
+     * <Component>                         -- <normal color> | <highlighted button color>
+     * Title screen background hex         -- 261941
+     * Start button hex                    -- 9EE788 | 689C65
+     * Options button hex                  -- CCAB6B | 907C73
+     * Exit button hex                     -- B44A4B | 65292A
+     * About button hex                    -- A687BC | 582760
+     * Credits button hex                  -- 8792BC | 273060
+     *
+     * Title screen title font: Unipix
+     * Title screen title size: 80
+     * About/Credit screen title coords: (-280, 170, 0)
+     * About/Credit screen text coords: (-237, -10, 0)
+     * About/Credit screen text size: 30
+     * About/Credit screen (all) text font: Unipix
+     *         (may make Credits text hard to read, let Ali know or
+     *                     change it to OpenSansRegular/Bold/SemiBold/etc.)
+     *
+     * Button size (All):
+     *     W: 160
+     *     H: 40
+     *
+     * About Button Coords (for Back Buttons on About/Credit/Option Scenes):
+     *     X = -290
+     *     Y = -200
+     *     Text Size: 18
+     *     Text Font: OpenSansRegular
+     *
+     * About Font Size:  
+     * About Text Size: 
+     */
+    
+    public void ToStart()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+    
+    
+    public void ToExit()
+    {
+        Application.Quit();
+    }
+
+    
+
+    public void ToOptions()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+
+
+    public void ToAbout()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+
+
+    public void ToCredits()
+    {
+        SceneManager.LoadScene(2);
+    }
+    
+}
